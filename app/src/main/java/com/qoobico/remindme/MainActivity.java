60f32ce0bf3520f2f5aa61148e2dcee716c43fa4
DmytroChernenko <dmytro.chernenko@ex.ua>
2016-01-03 03:13:13 +0200

    finish lesson #7 add item and header to NavigationView

--- app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.qoobico.remindme;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -11,16 +12,19 @@ import android.view.MenuItem;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private static final int LAYOUT = R.layout.activity_main;
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationViev();
     }
 
     private void initToolbar() {
@@ -34,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+
+    }
+
+    private void initNavigationViev() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
     }
 }
